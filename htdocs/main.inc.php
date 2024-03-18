45b36745e8b04a313bfe5d443521a8962ab809a3
Zephyriony <142790847+zephyriony@users.noreply.github.com>
2024-03-18 08:10:36 +0100

    Update main.inc.php

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -1694,7 +1694,6 @@ function top_httphead($contenttype = 'text/html', $forcenocache = 0)
 			$hookmanager = new HookManager($db);
 		}
 		$hookmanager->initHooks(array("main"));
-
 		$parameters = array('contentsecuritypolicy' => $contentsecuritypolicy, 'mode' => 'active');
 		$result = $hookmanager->executeHooks('setContentSecurityPolicy', $parameters); // Note that $action and $object may have been modified by some hooks
 		if ($result > 0) {
